a7e4f7352d9aa22126b5b4a915380a3e5aeae751
Tapan Sharma <sharma.tapan27@gmail.com>
2014-08-21 13:23:14 +0530

    First commit

---
+++ lib/lxcos/runner.rb
@@ -0,0 +1,7 @@
+require "lxcos/runner/version"
+
+module Lxcos
+  module Runner
+    # Your code goes here...
+  end
+end
---
+++ tasks/ready_for_the_day.rake
@@ -0,0 +1,3 @@
+task :turn_off_alarm do 
+  puts "Turned off alarm. Would like to have 5 more minutes, though."
+end
